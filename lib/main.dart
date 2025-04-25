4315adb7824e04ddbd5f8244c2dc17e14d0cd01c
AhmedAldeebOfficial <120066563+AhmedAldeebOfficial@users.noreply.github.com>
2025-04-26 02:52:10 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int? selectedIndex; // لتحديد أي زر تم الضغط عليه
+  int _selectedBottomIndex = 0; // متغير لحفظ فهرس العنصر في BottomNavigationBar
+
+  final List<Map<String, dynamic>> items = [
+    {'title': 'Buying', 'icon': Icons.shopping_cart},
+    {'title': 'Selling', 'icon': Icons.store},
+    {'title': 'Trades', 'icon': Icons.business_center},
+    {'title': 'Videos', 'icon': Icons.play_circle_fill},
+    {'title': 'Deals', 'icon': Icons.local_offer},
+    {'title': 'Case Study', 'icon': Icons.menu_book},
+  ];
+
+  void onItemTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100], // لون الخلفية الخفيف
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Vestimate',
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Choose your area',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                itemCount: items.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 1,
+                ),
+                itemBuilder: (context, index) {
+                  bool isSelected = selectedIndex == index;
+                  return GestureDetector(
+                    onTap: () => onItemTap(index),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isSelected ? Colors.blueAccent : Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 8,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            items[index]['icon'],
+                            size: 40,
+                            color: isSelected ? Colors.white : Colors.blueAccent,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            items[index]['title'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: isSelected ? Colors.white : Colors.blueAccent,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedBottomIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedBottomIndex = index;
+          });
+        },
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pages_outlined),
+            label: 'Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Contacts',
+          ),
+        ],
+      ),
+    );
+  }
+}
